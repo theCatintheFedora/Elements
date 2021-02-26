ae2e35014dba2867cba9783d8c8ffed89ab2e470
Timothy Price <theCatintheFedora1367@gmail.com>
2021-02-26 10:56:03 -0600

    I made some fancy code that works perfectly fine and then spits out an error when it is run, I feel so accomplished.

--- Elements/ContentView.swift
+++ Elements/ContentView.swift
@@ -8,10 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var elements = [Element]()
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(elements) { element in
+                NavigationLink(
+                    destination: Text(element.facts)
+                        .padding(),
+                    label: {
+                        Text(element.name)
+                    })
+            }
+            .navigationTitle("Elements information")
+            .onAppear(perform: {
+                getElements()
+            })
+            .alert(isPresented: $showingAlert, content: {
+                Alert(title: Text("Loading Error"),
+                      message: Text("There was a problem loading the data"),
+                      dismissButton: .default(Text("OK")))
+            })
+        }
     }
+    func getElements() {
+        let apiKey = "?rapidapi-key=ba1b549956msha6fa1902ee76913p1104c9jsncc2a41204a3d"
+        let query = "https://periodictable.p.rapidapi.com/?rapidapi-key=\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let name = item["name"].stringValue
+                        let facts = item["facts"].stringValue
+                        let element = Element(name: name, facts: facts)
+                        elements.append(element)
+                    }
+                    return
+                }
+            }
+        }
+        showingAlert = true
+    }
+}
+
+struct Element: Identifiable {
+    let id = UUID()
+    var name: String
+    var facts: String
 }
 
 struct ContentView_Previews: PreviewProvider {
