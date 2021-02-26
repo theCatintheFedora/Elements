53974d594463e91fdd5afb7b9e96f853bbff766e
Timothy Price <theCatintheFedora1367@gmail.com>
2021-02-26 10:27:25 -0600

    Initial Commit

---
+++ Elements/ElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementsApp.swift
+//  Elements
+//
+//  Created by Student on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
